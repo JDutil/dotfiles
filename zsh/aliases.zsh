ac615abe5301f063ec627c95fed35d7d622f31bc
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-12-02 02:38:19 -0500

    Add some customization.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,59 @@
 alias reload!='. ~/.zshrc'
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias be="bundle exec"
+alias amend="git commit --amend"
+alias head1="git rebase -i HEAD~1"
+alias ext_specs='bundle; be rake test_app; be rspec'
+
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
+
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+  then
+  alias git=$hub_path
+fi
+
+# The rest of my fun git aliases
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
+perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+
+function fix() {
+  git rebase -i HEAD~$1
+}
+
+function branch_name() {
+  branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+  branch_name=${branch_name##refs/heads/}
+  echo $branch_name
+}
+
+function spush() {
+  git push upstream `branch_name`
+}
+
+function spull() {
+  git pull --rebase upstream `branch_name`
+}
--- zsh/window.zsh
+++ zsh/window.zsh
@@ -17,3 +17,9 @@ function title() {
   esac
 }
 
+
+precmd () {
+  tab_label=${PWD/${HOME}/\~} # use 'relative' path
+  echo -ne "\e]2;${tab_label}\a" # set window title to full string
+  echo -ne "\e]1;${tab_label: -24}\a" # set tab title to rightmost 24 characters
+}
